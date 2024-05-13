2f91f74587692e398b0ea12e7fd6827655b475f9
AArvidd <arvid.oscarsson@elev.ga.ntig.se>
2024-05-13 10:17:13 +0200

    code added

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,19 @@ public class Main {
 
         MulticastSocket chatMulticastSocket = new MulticastSocket(portNumber);
 
+        InetAddress group = InetAddress.getByName("255.4.5.6");
+
+        chatMulticastSocket.joinGroup(group);
+
+        String mgs = "";
+        System.out.print("Type a message for the server: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        mgs = br.readLine();
+
+        DatagramPacket data = new DatagramPacket(mgs.getBytes(), 0, mgs.length(), group, portNumber);
+        chatMulticastSocket.send(data);
+
+        chatMulticastSocket.close();
+
     }
 }
